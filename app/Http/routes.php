65640e350710148e9d92187d1cdd3c241ad160fc
Olga--Smirnova <olgasmirnova@Olgas-MacBook-Pro.local>
2015-04-21 13:01:38 +1200

    the header is ready for you to use

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/', 'HomeController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
