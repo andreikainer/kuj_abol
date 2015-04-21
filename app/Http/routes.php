84a2343902378b0d5b9a01cdcf3b6c58acc8025b
Andrei Kainer <andreikainer@gmail.com>
2015-04-22 11:16:26 +1200

    added header from orign master

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+Route::get('/', function(){
+    return 'it works!';
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
