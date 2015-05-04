304d77acaa0f2eec788cfe48610bd0621087edb5
Olga--Smirnova <olgasmirnova@Olgas-MacBook-Pro.local>
2015-05-04 18:20:15 +1200

    Fixed bug for fadeIn/fadeOut of errors

---
+++ resources/lang/en/how-it-works.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | HOW IT WORKS PAGE
+    |--------------------------------------------------------------------------
+    |
+    | Language translation keys for the contact-page form.
+    */
+
+    'our-goal'              => 'Our goal',
+    'our-mission'           => 'Our mission',
+    'our-team'              => 'Our team',
+    'tips-for-success'      => 'Tips to success',
+];
--- resources/views/pages/how-it-works.blade.php
+++ resources/views/pages/how-it-works.blade.php
@@ -2,11 +2,21 @@
 
 @section('content')
 
-<div class="container-fluid" role="main">
+    <div class="container-fluid" role="main">
 
-            <div class="row">
-                how it works
+            {{--<div class="row">--}}
+                {{--how it works--}}
+            {{--</div>--}}
+
+        <div class="row">
+
+            <div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3 text-center mt-3em">
+                <h2 class="heading" id="our_goal">{{ trans('how-it-works-page.our-goal') }}</h2>
             </div>
+
+        </div>
+
+                    <div class="row">
 
 
 @endsection
